82f6ddbabb6a4604066118907fb88148662e9a2f
CasisBM <casis.bouissamatoko@gmail.com>
2022-05-09 11:30:28 +0200

    ajout maj php

--- 01_syntaxe/index.php
+++ 01_syntaxe/index.php
@@ -1,12 +1,61 @@
 <h1>Un Titre</h1>
 <?php
 //echo/print
-echo "texte en ph", " Autre Texte<br>" ;
-print "Texte en php avec print";
-//Print 'Texte en php avec print'
+
+echo "texte en ph", " Autre Texte <br/>";
+print "Texte en php avec print <br/>";
+
+//Print 'Texte en php avec print <br/>'
 //un commentaire
 # un commentaire
 /*
 *commentaire sur plusiuer ligne
 */
-?>
+
+// variable 
+//fonctionne
+
+$variable = "Une variable";
+$Variable = "Une autre variable";
+echo $variable, $Variable;
+$_3var = 34;
+echo $_3var;
+
+// fonctionne pas
+// $3var = 34;
+
+// string
+
+$nom = "mohamed";
+//int
+
+$nombre = 34;
+//float
+
+$nombreDecimale = 23.7;
+//bool
+
+$vrai = true;
+$faux = false;
+
+//array/objet
+
+$tableau = ["groupe", 34, 45.6,false];
+
+//null
+//$variable = null;
+
+// simple/double guillemets (quotes)
+
+//$message = "aujourhui"
+//$message = 'aujourd\'hui'
+$tkt = 'Bonjour';
+//echo "$txt tout le monde";
+echo '$txt tout le monde<br>';
+
+//concatenation
+
+$fruit = 'Kiwi';
+$fruit2 = 'Pomme';
+$fruit = 'Banabe'
+?>;
