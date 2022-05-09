bd0f8dc5947d8bfa42610effc88f882cf24acd79
CasisBM <casis.bouissamatoko@gmail.com>
2022-05-09 10:28:30 +0200

    ajout de commentaire

---
+++ 01_syntaxe/index.php
@@ -0,0 +1,12 @@
+<h1>Un Titre</h1>
+<?php
+//echo/print
+echo "texte en ph", " Autre Texte<br>" ;
+print "Texte en php avec print";
+//Print 'Texte en php avec print'
+//un commentaire
+# un commentaire
+/*
+*commentaire sur plusiuer ligne
+*/
+?>
