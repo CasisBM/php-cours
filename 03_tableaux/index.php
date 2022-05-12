ff8be63f162db0ca46bc1ddf71c97167b10c2f89
CasisBM <casis.bouissamatoko@gmail.com>
2022-05-12 11:50:00 +0200

    ajout d'une tableau et une boucle dans 03_tableaux

--- 03_tableaux/index.php
+++ 03_tableaux/index.php
@@ -1,11 +1,13 @@
 <?php 
+echo '<pre>';
+/*
 //Tableau indexé
 $tableau = array();
 $tableau = [];
 //$tableau = array('Lundi', 'Mardi', 45,67.8,false);
 $tableau = [true, 45, 'Terre',56,[45,67,78]];
 
-echo '<pre>';
+
 
 //print_r($tableau);
 $tableau[] = 'Janvier';
@@ -22,6 +24,38 @@ $notes = [12,5,16,7];
 $moyenne = array_sum($notes)/count($notes);
 //$tab3 = array_merge($tableau,$notes); // assemble des tableaux
 //var_dump($tab3);
-echo $moyenne;
-echo '<pre/>';
+echo var_dump($tableau);
+for ($i=0; $i < count($tableau); $i++) { 
+    # code...
+    echo $tableau[$i].'<br/>';
+}
+
+$fruits = ['Banane', 'Kiwi', 'Pomme','Poire'];
+$html ='';
+
+for ($i=0; $i < count($fruits); $i++) { 
+    # code...
+    echo $fruits[$i] . ',';
+}
+
+foreach($fruits as $fruit ){
+    # code...
+    echo $fruit . '<br/>';
+}
+*/
+// Construire un tableau $nombreq à l'aide de la boucle for contenant des nombres de 34 à 78
+
+$nombres = [];
+
+for ($i=34; $i <= 78; $i++) { 
+    # code...
+    array_push($nombres,$i);
+}
+
+echo(var_dump($nombres));
+
+
+//echo $moyenne;
+echo '</pre>';
+
 ?>
