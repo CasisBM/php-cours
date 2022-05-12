18b7e683f6a29d6687e823aad8c2f58737946e06
CasisBM <casis.bouissamatoko@gmail.com>
2022-05-12 14:40:03 +0200

    ajout exo Tableau Associatif

--- 03_tableaux/index.php
+++ 03_tableaux/index.php
@@ -42,7 +42,7 @@ foreach($fruits as $fruit ){
     # code...
     echo $fruit . '<br/>';
 }
-*/
+
 // Construire un tableau $nombreq à l'aide de la boucle for contenant des nombres de 34 à 78
 
 $nombres = [];
@@ -52,10 +52,49 @@ for ($i=34; $i <= 78; $i++) {
     array_push($nombres,$i);
 }
 
-echo(var_dump($nombres));
+//echo(var_dump($nombres));
 
+
+//Tableau Associatif
+$fruits = ['banane'=> 'jaune' ,'pomme' => 'rouge', 'kiwi'=>'vert'];
+echo 'La couleur de la pomme est ' . $fruits["pomme"];
+echo '<hr>';
+echo 'La longueur du tableau $fruits est de' . sizeof($fruits);
+//sizeof() = count()
+$ages = ['Michel'=> 34, 'Lucie' => 12, 'Didier'=>56];
+
+$chaine = implode('-', $ages);// assemble value d'un tableau en string
+$chaine2 =  explode('-', $chaine);// transforme un string en tableau en presisant le delimiteur
+print_r($chaine2);
+echo '<hr>';
+echo $chaine; 
+
+$ages['Paul'] = 22;
 
 //echo $moyenne;
+
+$html = '<ul>';
+foreach ($ages as $key => $value) {
+    # code...
+    $html .= '<li>' . $key . ' a ' . $value . ' ans</li>';
+}
+$html .= '</ul>';
+echo $html;
+*/
+
+//Creer un tableau $notes avec un prenom => une note
+//Afficher une liste ul des eleves et les notes
+//Marc a eu 15/20
+
+$notes = ['Michel'=> 12, 'Lucie' => 5, 'Didier'=>16, 'Marc' =>15];
+$html = '<ul>';
+foreach ($notes as $key => $value) {
+    # code...
+    $html .= '<li>' . $key . ' a eu ' . $value . '/20</li>';
+}
+$html .= '</ul>';
+echo $html;
+
 echo '</pre>';
 
 ?>
