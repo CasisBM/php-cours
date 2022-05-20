d20368ca9670789387d0e71a68fbfcd767c24f5c
Khazise <71380558+Khazise@users.noreply.github.com>
2022-05-20 10:39:27 +0200

    travail-01 fini

--- travaux/travail-01.php
+++ travaux/travail-01.php
@@ -14,3 +14,40 @@ Travail-01 :
 | 11 | 51.8 |
  */
 
+ $tabCelsius = [25,03,35,11];
+ $tabFahrenheit=[];
+
+ foreach ($tabCelsius as $key => $value) {
+     # code...
+     //var_dump($value);
+     array_push($tabFahrenheit,($value * 9/5) + 32);
+ }
+ $tab = array_combine($tabCelsius, $tabFahrenheit);
+ //var_dump($tab);
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+     <table border="1">
+        <tr>
+            <th>Celsius °C</th>
+            <th>Fahrenheit °F</th>
+        </tr>
+
+        <?php foreach ($tab as $key => $value) {?>
+            <tr>
+                <td> <?=$key ?></td>
+                <td> <?=$value ?></td>
+            </tr>
+            <?php }?>
+     </table>
+ </body>
+ </html>
